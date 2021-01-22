2622a19985ca00cbf37b9203eb7f951cdba657d6
Maria Rubinger <rubinger.maria@gmail.com>
2021-01-22 15:17:32 +0000

    Renderizar condicional

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
+import DadosEntrega from "./DadosEntrega";
+import { Typography } from '@material-ui/core';
 
 //esse formulario vai controlar quantos cadastros a gente vai ter
-function FormularioCadastro({aoEnviar, validarCPF}) {
-
+function FormularioCadastro({ aoEnviar, validarCPF }) {
+  const [etapaAtual, setEtapaAtual] = useState(1);
+  
+  function formularioAtual(etapa) {
+    switch (etapa) {
+      case 0:
+        return <DadosUsuario />;
+      case 1:
+        return <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} />;
+      case 2:
+        return <DadosEntrega />;
+      default:
+        return <Typography>Erro ao selecionar formulário!</Typography>
+    }
+  }    
+  
   return (
     <>
-    <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF}/>
-    <DadosUsuario/>
+    {formularioAtual(etapaAtual)}
     </>
   );
 }
+
+
+//SwitchCase é possível ser colocado dentro de uma função do JS
+//não é possível colocá-lo sozinho no react
+//
 
 export default FormularioCadastro;
